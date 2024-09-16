90b835f968187545202d636c59ce0938b6bd7ff0
lewisblackburn <lewisblackburn10@gmail.com>
2024-09-16 13:40:35 +0100

    fix: better sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,33 @@
-/**
- * @type {import('next-sitemap').IConfig}
- * @see https://github.com/iamvishnusankar/next-sitemap#readme
- */
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
-  // !STARTERCONF Change the siteUrl
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: 'https://drive2learn.co.uk',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
   generateRobotsTxt: true,
+  exclude: ['/dashboard', '/login'],
+  alternateRefs: [
+    {
+      href: 'https://drivetolearn.co.uk',
+    },
+  ],
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
   robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
   },
 };
