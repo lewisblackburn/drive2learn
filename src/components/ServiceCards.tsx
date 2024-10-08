44a77350ed9246a4bdb60f22d6fade67148cffce
lewisblackburn <lewisblackburn10@gmail.com>
2024-10-08 12:58:16 +0100

    service cards all the same height

--- src/components/ServiceCards.tsx
+++ src/components/ServiceCards.tsx
@@ -84,16 +84,18 @@ const ServiceCard = ({
   active: boolean;
 }) => {
   return (
-    <div className='w-full px-4 md:w-1/2 lg:w-1/3'>
-      <div className='relative z-10 mb-10 overflow-hidden rounded-[10px] border-2 border-stroke bg-white px-8 py-10 shadow-pricing sm:p-12 lg:px-6 lg:py-10 xl:p-[50px]'>
-        <span className='mb-3 block text-lg font-semibold text-primary'>
-          {type}
-        </span>
-        <h2 className='mb-5 text-[42px] font-bold text-dark '>{price}</h2>
-        <p className='mb-8 border-b border-stroke pb-8 text-base text-body-color'>
-          {description}
-        </p>
-        <div className='mb-9 flex flex-col gap-[14px]'>{children}</div>
+    <div className='w-full px-4 md:w-1/2 lg:w-1/3 flex'>
+      <div className='relative z-10 mb-10 flex flex-col justify-between overflow-hidden rounded-[10px] border-2 border-stroke bg-white px-8 py-10 shadow-pricing sm:p-12 lg:px-6 lg:py-10 xl:p-[50px]'>
+        <div>
+          <span className='mb-3 block text-lg font-semibold text-primary'>
+            {type}
+          </span>
+          <h2 className='mb-5 text-[42px] font-bold text-dark'>{price}</h2>
+          <p className='mb-8 border-b border-stroke pb-8 text-base text-body-color'>
+            {description}
+          </p>
+          <div className='mb-9 flex flex-col gap-[14px]'>{children}</div>
+        </div>
         <Link
           href={buttonLink}
           className={` ${
